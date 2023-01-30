1fcd53eb2677a58840646a489af8d71ae23d5e64
恵太 川満 <keitakawamitsu0980@gmail.com>
2023-01-30 20:20:58 +0900

    [update]ボタンにカーブ生成の仮メソッド追加

--- kmt_controller.py
+++ kmt_controller.py
@@ -104,41 +104,57 @@ class Tab1Widget(QWidget):
 class Tab2Widget(QWidget):
     def __init__(self, parent=None):
         super(Tab2Widget, self).__init__()
-        self.icon_path  = os.path.dirname(__file__)+"/icons"
+        self.icon_path1 = path.get_image_paths("sub")
+
+        self.icon_path  = os.path.dirname(__file__)+"/icons/sub"
         self.btn_size = 37
+
+        self.cv_SSSSquare = curves.Curves("_Square")
+
+
         self.button()
     def button(self):
         self.vbox = QVBoxLayout()
 
-        pixmap_05 = QPixmap(self.icon_path  + "/KK_Controller_Dia")
+        pixmap_05 = QPixmap(self.icon_path1[0])
         scaled_pixmap_05 = pixmap_05.scaled(self.btn_size, self.btn_size, Qt.KeepAspectRatio)
         self.tool_button05 = QPushButton()
         self.tool_button05.setIcon(scaled_pixmap_05)
         self.tool_button05.setIconSize(QSize(self.btn_size,self.btn_size))
+        self.tool_button05.clicked.connect(self.cv_SSSSquare.make_curves)
 
-        pixmap_06 = QPixmap(self.icon_path  + "/KK_Controller_Dia")
+
+        pixmap_06 = QPixmap(self.icon_path1[1])
         scaled_pixmap_06 = pixmap_06.scaled(self.btn_size, self.btn_size, Qt.KeepAspectRatio)
         self.tool_button06 = QPushButton()
         self.tool_button06.setIcon(scaled_pixmap_06)
         self.tool_button06.setIconSize(QSize(self.btn_size,self.btn_size))
+        self.tool_button06.clicked.connect(self.cv_SSSSquare.make_curves)
 
-        pixmap_07 = QPixmap(self.icon_path  + "/KK_Controller_Dia")
+
+        pixmap_07 = QPixmap(self.icon_path1[2])
         scaled_pixmap_07 = pixmap_07.scaled(self.btn_size, self.btn_size, Qt.KeepAspectRatio)
         self.tool_button07 = QPushButton()
         self.tool_button07.setIcon(scaled_pixmap_07)
         self.tool_button07.setIconSize(QSize(self.btn_size,self.btn_size))
+        self.tool_button07.clicked.connect(self.cv_SSSSquare.make_curves)
 
-        pixmap_08 = QPixmap(self.icon_path  + "/KK_Controller_Dia")
+
+        pixmap_08 = QPixmap(self.icon_path1[3])
         scaled_pixmap_08 = pixmap_08.scaled(self.btn_size, self.btn_size, Qt.KeepAspectRatio)
         self.tool_button08 = QPushButton()
         self.tool_button08.setIcon(scaled_pixmap_08)
         self.tool_button08.setIconSize(QSize(self.btn_size,self.btn_size))
+        self.tool_button08.clicked.connect(self.cv_SSSSquare.make_curves)
 
-        pixmap_09 = QPixmap(self.icon_path  + "/KK_Controller_Dia")
+        pixmap_09 = QPixmap(self.icon_path1[4])
         scaled_pixmap_09 = pixmap_09.scaled(self.btn_size, self.btn_size, Qt.KeepAspectRatio)
         self.tool_button09 = QPushButton()
         self.tool_button09.setIcon(scaled_pixmap_09)
         self.tool_button09.setIconSize(QSize(self.btn_size,self.btn_size))
+        self.tool_button09.clicked.connect(self.cv_SSSSquare.make_curves)
+
+
 
         hbox = QHBoxLayout()
         hbox.addWidget(self.tool_button05)
@@ -147,35 +163,43 @@ class Tab2Widget(QWidget):
         hbox.addWidget(self.tool_button08)
         hbox.addWidget(self.tool_button09)
 
-        pixmap_10 = QPixmap(self.icon_path  + "/KK_Controller_Dia")
+        pixmap_10 = QPixmap(self.icon_path1[5])
         scaled_pixmap_10 = pixmap_10.scaled(self.btn_size, self.btn_size, Qt.KeepAspectRatio)
         self.tool_button10 = QPushButton()
         self.tool_button10.setIcon(scaled_pixmap_10)
         self.tool_button10.setIconSize(QSize(self.btn_size,self.btn_size))
+        self.tool_button10.clicked.connect(self.cv_SSSSquare.make_curves)
         
-        pixmap_11 = QPixmap(self.icon_path  + "/KK_Controller_Dia")
+
+
+        pixmap_11 = QPixmap(self.icon_path1[6])
         scaled_pixmap_11 = pixmap_11.scaled(self.btn_size, self.btn_size, Qt.KeepAspectRatio)
         self.tool_button11 = QPushButton()
         self.tool_button11.setIcon(scaled_pixmap_11)
         self.tool_button11.setIconSize(QSize(self.btn_size,self.btn_size))
+        self.tool_button11.clicked.connect(self.cv_SSSSquare.make_curves)
 
-        pixmap_12 = QPixmap(self.icon_path  + "/KK_Controller_Dia")
+
+        pixmap_12 = QPixmap(self.icon_path1[7])
         scaled_pixmap_12 = pixmap_12.scaled(self.btn_size, self.btn_size, Qt.KeepAspectRatio)
         self.tool_button12 = QPushButton()
         self.tool_button12.setIcon(scaled_pixmap_12)
         self.tool_button12.setIconSize(QSize(self.btn_size,self.btn_size))
+        self.tool_button12.clicked.connect(self.cv_SSSSquare.make_curves)
 
-        pixmap_13 = QPixmap(self.icon_path  + "/KK_Controller_Dia")
+        pixmap_13 = QPixmap(self.icon_path1[8])
         scaled_pixmap_13 = pixmap_13.scaled(self.btn_size, self.btn_size, Qt.KeepAspectRatio)
         self.tool_button13 = QPushButton()
         self.tool_button13.setIcon(scaled_pixmap_13)
         self.tool_button13.setIconSize(QSize(self.btn_size,self.btn_size))
+        self.tool_button13.clicked.connect(self.cv_SSSSquare.make_curves)
 
-        pixmap_14 = QPixmap(self.icon_path  + "/KK_Controller_Dia")
+        pixmap_14 = QPixmap(self.icon_path1[9])
         scaled_pixmap_14 = pixmap_14.scaled(self.btn_size, self.btn_size, Qt.KeepAspectRatio)
         self.tool_button14 = QPushButton()
         self.tool_button14.setIcon(scaled_pixmap_14)
         self.tool_button14.setIconSize(QSize(self.btn_size,self.btn_size))
+        self.tool_button14.clicked.connect(self.cv_SSSSquare.make_curves)
 
         hbox1 = QHBoxLayout()
         hbox1.addWidget(self.tool_button10)
@@ -184,36 +208,41 @@ class Tab2Widget(QWidget):
         hbox1.addWidget(self.tool_button13)
         hbox1.addWidget(self.tool_button14)
 
-        pixmap_15 = QPixmap(self.icon_path  + "/KK_Controller_Dia")
+        pixmap_15 = QPixmap(self.icon_path1[10])
         scaled_pixmap_15 = pixmap_15.scaled(self.btn_size, self.btn_size, Qt.KeepAspectRatio)
         self.tool_button15 = QPushButton()
         self.tool_button15.setIcon(scaled_pixmap_15)
         self.tool_button15.setIconSize(QSize(self.btn_size,self.btn_size))
+        self.tool_button15.clicked.connect(self.cv_SSSSquare.make_curves)
         
-        pixmap_16 = QPixmap(self.icon_path  + "/KK_Controller_Dia")
+        pixmap_16 = QPixmap(self.icon_path1[11])
         scaled_pixmap_16 = pixmap_16.scaled(self.btn_size, self.btn_size, Qt.KeepAspectRatio)
         self.tool_button16 = QPushButton()
         self.tool_button16.setIcon(scaled_pixmap_16)
         self.tool_button16.setIconSize(QSize(self.btn_size,self.btn_size))
+        self.tool_button16.clicked.connect(self.cv_SSSSquare.make_curves)
 
 
-        pixmap_17 = QPixmap(self.icon_path  + "/KK_Controller_Dia")
+        pixmap_17 = QPixmap(self.icon_path1[12])
         scaled_pixmap_17 = pixmap_17.scaled(self.btn_size, self.btn_size, Qt.KeepAspectRatio)
         self.tool_button17 = QPushButton()
         self.tool_button17.setIcon(scaled_pixmap_17)
         self.tool_button17.setIconSize(QSize(self.btn_size,self.btn_size))
+        self.tool_button17.clicked.connect(self.cv_SSSSquare.make_curves)
 
-        pixmap_18 = QPixmap(self.icon_path  + "/KK_Controller_Dia")
+        pixmap_18 = QPixmap(self.icon_path1[13])
         scaled_pixmap_18 = pixmap_18.scaled(self.btn_size, self.btn_size, Qt.KeepAspectRatio)
         self.tool_button18 = QPushButton()
         self.tool_button18.setIcon(scaled_pixmap_18)
         self.tool_button18.setIconSize(QSize(self.btn_size,self.btn_size))
+        self.tool_button18.clicked.connect(self.cv_SSSSquare.make_curves)
 
-        pixmap_19 = QPixmap(self.icon_path  + "/KK_Controller_Dia")
+        pixmap_19 = QPixmap(self.icon_path1[14])
         scaled_pixmap_19 = pixmap_19.scaled(self.btn_size, self.btn_size, Qt.KeepAspectRatio)
         self.tool_button19 = QPushButton()
         self.tool_button19.setIcon(scaled_pixmap_19)
         self.tool_button19.setIconSize(QSize(self.btn_size,self.btn_size))
+        self.tool_button19.clicked.connect(self.cv_SSSSquare.make_curves)
 
         hbox2 = QHBoxLayout()
         hbox2.addWidget(self.tool_button15)
@@ -222,35 +251,40 @@ class Tab2Widget(QWidget):
         hbox2.addWidget(self.tool_button18)
         hbox2.addWidget(self.tool_button19)
 
-        self.pixmap_20 = QPixmap(self.icon_path  + "/KK_Controller_Dia")
+        self.pixmap_20 = QPixmap(self.icon_path1[15])
         self.scaled_pixmap_20 = self.pixmap_20.scaled(self.btn_size, self.btn_size, Qt.KeepAspectRatio)
         self.tool_button20 = QPushButton()
         self.tool_button20.setIcon(self.scaled_pixmap_20)
         self.tool_button20.setIconSize(QSize(self.btn_size,self.btn_size))
+        self.tool_button20.clicked.connect(self.cv_SSSSquare.make_curves)
 
-        self.pixmap_21 = QPixmap(self.icon_path  + "/KK_Controller_Dia")
+        self.pixmap_21 = QPixmap(self.icon_path1[16])
         self.scaled_pixmap_21 = self.pixmap_21.scaled(self.btn_size, self.btn_size, Qt.KeepAspectRatio)
         self.tool_button21 = QPushButton()
         self.tool_button21.setIcon(self.scaled_pixmap_21)
         self.tool_button21.setIconSize(QSize(self.btn_size,self.btn_size))
+        self.tool_button21.clicked.connect(self.cv_SSSSquare.make_curves)
 
-        self.pixmap_22 = QPixmap(self.icon_path  + "/KK_Controller_Dia")
+        self.pixmap_22 = QPixmap(self.icon_path1[17])
         self.scaled_pixmap_22 = self.pixmap_22.scaled(self.btn_size, self.btn_size, Qt.KeepAspectRatio)
         self.tool_button22 = QPushButton()
         self.tool_button22.setIcon(self.scaled_pixmap_22)
         self.tool_button22.setIconSize(QSize(self.btn_size,self.btn_size))
+        self.tool_button22.clicked.connect(self.cv_SSSSquare.make_curves)
 
-        self.pixmap_23 = QPixmap(self.icon_path  + "/KK_Controller_Dia")
+        self.pixmap_23 = QPixmap(self.icon_path1[18])
         self.scaled_pixmap_23 = self.pixmap_23.scaled(self.btn_size, self.btn_size, Qt.KeepAspectRatio)
         self.tool_button23 = QPushButton()
         self.tool_button23.setIcon(self.scaled_pixmap_23)
         self.tool_button23.setIconSize(QSize(self.btn_size,self.btn_size))
+        self.tool_button23.clicked.connect(self.cv_SSSSquare.make_curves)
 
-        self.pixmap_24 = QPixmap(self.icon_path  + "/KK_Controller_Dia")
+        self.pixmap_24 = QPixmap(self.icon_path1[19])
         self.scaled_pixmap_24 = self.pixmap_24.scaled(self.btn_size, self.btn_size, Qt.KeepAspectRatio)
         self.tool_button24 = QPushButton()
         self.tool_button24.setIcon(self.scaled_pixmap_24)
         self.tool_button24.setIconSize(QSize(self.btn_size,self.btn_size))
+        self.tool_button24.clicked.connect(self.cv_SSSSquare.make_curves)
 
 
         hbox3 = QHBoxLayout()
